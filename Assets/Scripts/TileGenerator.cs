3b3a82769abb4f7545d748b913c02f9b2bf7d27d
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-08-22 21:12:42 -0700

    Added SteamVR and began tile generation in home farm

---
+++ Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject tile;
+
+    public Vector2 size;
+
+    private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+        CreateTerrain();
+	}
+
+    private void CreateTerrain()
+    {
+        float length = size.x;
+        float width = size.y; 
+        for (int currLen = 0; currLen < length; currLen++)
+        {
+            GameObject.Instantiate(tile, )
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
