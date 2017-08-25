00bbaa61df064b267647ad894d2af5ba2aa9d218
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-08-24 22:31:21 -0700

    Added Tile object and generator creates terrain given size x and y.

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+// Summary: Tile object class for Terrain and TileGeneration
+// Author: Kristin Agcaoili
+// Date: 08/24/2017
+public class Tile : MonoBehaviour {
+
+}
--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -1,33 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+// Summary: Creates a length X and width Y terrain by instantiating a given prefab tile. Terrain is centered at (0,0,0).
+// Author: Kristin Agcaoili
+// Date: 08/24/2017
 public class TileGenerator : MonoBehaviour {
 
     [SerializeField]
-    private GameObject tile;
+    private Tile tile; // Holds the prefab tile
 
-    public Vector2 size;
+    public Vector2 size; // The size X and Y width and length of the terrain to create
 
-    private Vector3 startPosition;
+    private Vector3 startPosition; // The starting position dependent on the size of the terrain
+    private float tileRadius; // Radius of the tile. If default 1.0f, then the radius is 0.5f to offset correctly
 
-	// Use this for initialization
-	void Start () {
-        CreateTerrain();
+    // Summary: Initializes starting position and the tile radius. Calls CreateTerrain.
+    //          Starting position is NOT 0,0,0. The starting position is the Vector3 in which the center of the terrain (middle tile) is at 0,0,0.
+	void Start ()
+    {
+        startPosition = new Vector3(-(size.x / 2.0f), 0.0f, -(size.y / 2.0f)); // Shifts the starting position to the negative area by half the size of the terrain
+        if (tile)
+        {
+            tileRadius = tile.gameObject.transform.localScale.x / 2.0f;
+            CreateTerrain();
+        } else
+        {
+            Debug.LogWarning("TileGenerator: Tile not found.");
+        }
 	}
 
+    // Summary: Iterates through the given length and width and instantiates the tile by offsetting with the radius of the tile and the # tile being created.
     private void CreateTerrain()
     {
         float length = size.x;
-        float width = size.y; 
-        for (int currLen = 0; currLen < length; currLen++)
+        float width = size.y;
+
+        if (length == 0.0f && width == 0.0f)
         {
-            GameObject.Instantiate(tile, )
+            Debug.LogWarning("TileGenerator: Length and Width are zero. No terrain is created.");
+        }
+
+        Vector3 currPosition;
+        for (int currLenTile = 0; currLenTile < length; currLenTile++)
+        {
+            for (int currWidTile = 0; currWidTile < width; currWidTile++)
+            {
+                currPosition = new Vector3(startPosition.x + currLenTile + tileRadius, startPosition.y, startPosition.z + currWidTile + tileRadius);
+                GameObject newTile = GameObject.Instantiate(tile.gameObject, currPosition, Quaternion.identity);
+                newTile.transform.parent = gameObject.transform;
+            }
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
